6eb16f6259a25f0c40f687f922baae4269d161d6
Soumya-2184 <soumyagupta2015@gmail.com>
2026-01-23 18:09:21 +0530

    Added BookingService

--- JEDI_BRAVO_FLIPFIT_PROJECT_DEVELOPMENT/src/com/flipfit/business/BookingService.java
+++ JEDI_BRAVO_FLIPFIT_PROJECT_DEVELOPMENT/src/com/flipfit/business/BookingService.java
@@ -1,5 +1,13 @@
 package com.flipfit.business;
 
-public interface BookingService {
+import java.util.Date;
+import java.time.LocalTime;
 
-}
+public interface BookingService {
+    // Standard creation and cancellation
+	String createBooking(String userId, String centreId, LocalTime startTime, LocalTime endTime, Date date);
+    
+    boolean cancelBooking(String bookingId);
+    
+    boolean modifyBooking(String userId, String oldBookingId, LocalTime startTime, LocalTime endTime, Date date);
+}    
